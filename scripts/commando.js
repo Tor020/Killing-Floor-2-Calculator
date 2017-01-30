893c615c03bb56927f2750197f1d5f94da27a53d
Kevin <Tor000020@gmail.com>
2017-01-30 04:56:54 -0500

    Revert "Revert "Revert "Updated Large amounts of the website"""
    This reverts commit 3beece3601695378cba7b1fe8e35fdfb99d6632d.

--- scripts/commando.js
+++ scripts/commando.js
@@ -5,120 +5,126 @@ ROUNDED or ROUNDED UP -
 ROUNDED for Berserker, Demolitionist, Firebug, SWAT and 
 ROUNDED UP for Commando, Support, Gunslinger and Sharpshooter.				
 */
-//base damage of each of the core guns
-var varm = 30;
-var ak47 = 40;
-var scar = 50;
-//perk 
-var hpr = .25;
-//perk level
-var plvl = $("#perkLVL").val();
-//variable initializations for calculations
-var calc1 = 0;
-var calc2 = 0;
-var calc3 = 0;
+	//base damage of each of the core guns
+	var varm = 30;
+	var ak47 = 40;
+	var scar = 50;
+	//perk 
+	var hpr = .25;
+	//perk level
+	var plvl = $("#perkLVL").val();
+	//variable initializations for calculations
+	var calc1 = 0;
+	var calc2 = 0;
+	var calc3 = 0;
 
-// Shorthand for $( document ).ready()
-$(function() {
-    {
-        plvl = $("#perkLVL").val();
-        plvl = plvl * .01;
+	// Shorthand for $( document ).ready()
+	$(function(){
+		    {
+				plvl = $( "#perkLVL" ).val();
+				plvl = plvl * .01;
 
-        calc1 = (varm + (plvl * varm));
-        calc2 = (ak47 + (plvl * ak47));
-        calc3 = (scar + (plvl * scar));
+		      	calc1 = (varm + (plvl * varm));
+		      	calc2 = (ak47 + (plvl * ak47));
+		      	calc3 = (scar + (plvl * scar));
 
-        calc1 = Math.ceil(calc1);
-        calc2 = Math.ceil(calc2);
-        calc3 = Math.ceil(calc3);
+		      	calc1 = Math.ceil(calc1);
+		      	calc2 = Math.ceil(calc2);
+		      	calc3 = Math.ceil(calc3);
+		      	
+				$(".res1").html(calc1);	
+				$(".res2").html(calc2);
+				$(".res3").html(calc3);	
+		    }
 
-        $(".res1").html(calc1);
-        $(".res2").html(calc2);
-        $(".res3").html(calc3);
-    }
+			});
 
-});
+		//this fucntion prevents inputs more than 25
+		$('#perkLVL').on('keyup keydown', function(e){
+	    console.log($(this).val() > 25)
+	        if 
+	        	//e.keyCode != 46 aka delete
+	        	//e.keyCode != 8 aka backspace
+				($(this).val() > 25 && e.keyCode != 46 && e.keyCode != 8) {
+	        	e.preventDefault();     
+	        	$(this).val(25);
+	        }
+	    });
 
-//this fucntion prevents inputs more than 25
-$('#perkLVL').on('keyup keydown', function(e) {
-    console.log($(this).val() > 25)
-    if
-    //e.keyCode != 46 aka delete
-    //e.keyCode != 8 aka backspace
-    ($(this).val() > 25 && e.keyCode != 46 && e.keyCode != 8) {
-        e.preventDefault();
-        $(this).val(25);
-    }
-});
+		$("#perkLVL").on("click change blur focus",function() {
+		 	if ($('#chkHPR').is(':checked')) {
+		 		plvl = $("#perkLVL").val();
+		 		console.log(plvl);
+				plvl = plvl*.01;
+				console.log(plvl)
 
-$("#perkLVL").on("click change blur focus", function() {
-    if ($('#chkHPR').is(':checked')) {
-        plvl = $("#perkLVL").val();
-        console.log(plvl);
-        plvl = plvl * .01;
-        console.log(plvl)
+				calc1 = (varm + (hpr * varm) + (plvl * varm));
+				console.log((varm + (hpr * varm) + (plvl * varm)));
+				calc2 = (ak47 + (hpr * ak47) + (plvl * ak47));
+				calc3 = (scar + (hpr * scar) + (plvl * scar));
+		      	calc1 = Math.ceil(calc1);
+		      	calc2 = Math.ceil(calc2);
+		      	calc3 = Math.ceil(calc3);
+		      	
+				$(".res1").html(calc1);	
+				$(".res2").html(calc2);
+				$(".res3").html(calc3);	
+		    } 
+		    else 
+		    {
+				plvl = $( "#perkLVL" ).val();
+				plvl = plvl * .01;
 
-        calc1 = (varm + (hpr * varm) + (plvl * varm));
-        console.log((varm + (hpr * varm) + (plvl * varm)));
-        calc2 = (ak47 + (hpr * ak47) + (plvl * ak47));
-        calc3 = (scar + (hpr * scar) + (plvl * scar));
-        calc1 = Math.ceil(calc1);
-        calc2 = Math.ceil(calc2);
-        calc3 = Math.ceil(calc3);
+		      	calc1 = (varm + (plvl * varm));
+		      	calc2 = (ak47 + (plvl * ak47));
+		      	calc3 = (scar + (plvl * scar));
 
-        $(".res1").html(calc1);
-        $(".res2").html(calc2);
-        $(".res3").html(calc3);
-    } else {
-        plvl = $("#perkLVL").val();
-        plvl = plvl * .01;
-
-        calc1 = (varm + (plvl * varm));
-        calc2 = (ak47 + (plvl * ak47));
-        calc3 = (scar + (plvl * scar));
-
-        calc1 = Math.ceil(calc1);
-        calc2 = Math.ceil(calc2);
-        calc3 = Math.ceil(calc3);
-
-        $(".res1").html(calc1);
-        $(".res2").html(calc2);
-        $(".res3").html(calc3);
-    }
-});
-
-
-$("#chkHPR").change(function() {
-    if ($('#chkHPR').is(':checked')) {
-        plvl = $("#perkLVL").val();
-        plvl = plvl * .01;
-
-        calc1 = (varm + (hpr * varm) + (plvl * varm));
-        calc2 = (ak47 + (hpr * ak47) + (plvl * ak47));
-        calc3 = (scar + (hpr * scar) + (plvl * scar));
+		      	calc1 = Math.ceil(calc1);
+		      	calc2 = Math.ceil(calc2);
+		      	calc3 = Math.ceil(calc3);
+		      	
+				$(".res1").html(calc1);	
+				$(".res2").html(calc2);
+				$(".res3").html(calc3);	
+		    }
+		});
 
 
-        calc1 = Math.ceil(calc1);
-        calc2 = Math.ceil(calc2);
-        calc3 = Math.ceil(calc3);
+		$("#chkHPR").change(function() {
+		 	if 
+		 		($('#chkHPR').is(':checked')) {
+		 		plvl = $("#perkLVL").val();
+				plvl = plvl*.01;
 
-        $(".res1").html(calc1);
-        $(".res2").html(calc2);
-        $(".res3").html(calc3);
-    } else {
-        plvl = $("#perkLVL").val();
-        plvl = plvl * .01;
+				calc1 = (varm + (hpr * varm) + (plvl * varm));
+				calc2 = (ak47 + (hpr * ak47) + (plvl * ak47));
+				calc3 = (scar + (hpr * scar) + (plvl * scar));
 
-        calc1 = (varm + (plvl * varm));
-        calc2 = (ak47 + (plvl * ak47));
-        calc3 = (scar + (plvl * scar));
 
-        calc1 = Math.ceil(calc1);
-        calc2 = Math.ceil(calc2);
-        calc3 = Math.ceil(calc3);
+		      	calc1 = Math.ceil(calc1);
+		      	calc2 = Math.ceil(calc2);
+		      	calc3 = Math.ceil(calc3);
 
-        $(".res1").html(calc1);
-        $(".res2").html(calc2);
-        $(".res3").html(calc3);
-    }
-});
+				$(".res1").html(calc1);	
+				$(".res2").html(calc2);
+				$(".res3").html(calc3);	
+		    } 
+		    else 
+		    {
+				plvl = $( "#perkLVL" ).val();
+				plvl = plvl * .01;
+
+		      	calc1 = (varm + (plvl * varm));
+		      	calc2 = (ak47 + (plvl * ak47));
+		      	calc3 = (scar + (plvl * scar));
+
+		      	calc1 = Math.ceil(calc1);
+		      	calc2 = Math.ceil(calc2);
+		      	calc3 = Math.ceil(calc3);
+
+				$(".res1").html(calc1);	
+				$(".res2").html(calc2);
+				$(".res3").html(calc3);	
+		    }
+		});
+
