bd5f77fe1d77bb1961ecdb7572cf1c93e507fa19
Kevin <Tor000020@gmail.com>
2017-03-08 17:29:42 -0500

    fixed a bug involving butcher and massacre not working when checked

--- scripts/berserker.js
+++ scripts/berserker.js
@@ -188,6 +188,72 @@ $("#perkLVL, #chkbutch, #chksmash, #chkmass").on("click change blur focus", func
         $(".ng1").html(vladc);
         $(".ng2").html(vladc*7);
     } 
+
+    //checking for mass and butcher being checked
+    else if ($('#chkbutch').is(':checked') && $('#chkmass').is(':checked')) {
+
+        plvl = $("#perkLVL").val();
+        plvl = plvl * .01;
+
+        cstcalc1 = cstl + (plvl * cstl) + (butch * cstl) + (mass * cstl);
+        cstcalc2 = csth + (plvl * csth) + (butch * csth);
+        cstcalc3 = cstm + (plvl * cstm) + (butch * cstm);
+        katcalc1 = kat1 + (plvl * kat1) + (butch * kat1) + (mass * kat1);
+        katcalc2 = kat2 + (plvl * kat2) + (butch * kat2);
+        pulcalc1 = pull + (plvl * pull) + (butch * pull) + (mass * pull);
+        pulcalc2 = pulh + (plvl * pulh) + (butch * pulh);
+        pulcalc3 = pulm + (plvl * pulm) + (butch * pulm);
+        zweic1 = zwel + (plvl * zwel) + (butch * zwel) + (mass * zwel);
+        zweic2 = zweh + (plvl * zweh) + (butch * zweh);
+        zweic3 = zwem + (plvl * zwem) + (butch * zwem);
+        evi1c = evil + (plvl * evil) + (butch * evil);
+        evihc = evih + (plvl * evih) + (butch * evih);
+        evimc = evim + (plvl * evim) + (butch * evim);
+        bclc = bcl + (plvl * bcl) + (butch * bcl) + (mass * bcl);
+        bchc = bch + (plvl * bch) + (butch * bch);
+        bcmc = bcm + (plvl * bcm) + (butch * bcm);
+        vladc  = vlad + (plvl * vlad) + (butch * vlad);
+
+        cstcalc1 = Math.round(cstcalc1);
+        cstcalc2 = Math.round(cstcalc2);
+        cstcalc3 = Math.round(cstcalc3);
+        katcalc1 = Math.round(katcalc1);
+        katcalc2 = Math.round(katcalc2);
+        pulcalc1 = Math.round(pulcalc1);
+        pulcalc2 = Math.round(pulcalc2);
+        pulcalc3 = Math.round(pulcalc3);
+        zweic1 = Math.round(zweic1);
+        zweic2 = Math.round(zweic2);
+        zweic3 = Math.round(zweic3);
+        evi1c = Math.round(evi1c);
+        evihc = Math.round(evihc);
+        evimc = Math.round(evimc);
+        bclc = Math.round(bclc);
+        bchc = Math.round(bchc);
+        bcmc = Math.round(bcmc);
+        vladc = Math.round(vladc);
+
+        $(".cst1").html(cstcalc1);
+        $(".cst2").html(cstcalc2);
+        $(".cst3").html(cstcalc3);
+        $(".kat1").html(katcalc1);
+        $(".kat2").html(katcalc2);
+        $(".kat3").html(katcalc1);
+        $(".pulv1").html(pulcalc1);
+        $(".pulv2").html(pulcalc2);
+        $(".pulv3").html(pulcalc3);
+        $(".zwei1").html(zweic1);
+        $(".zwei2").html(zweic2);
+        $(".zwei3").html(zweic3);
+        $(".evis1").html(evi1c);
+        $(".evis2").html(evihc);
+        $(".evis3").html(evimc);
+        $(".bc1").html(bclc);
+        $(".bc2").html(bchc);
+        $(".bc3").html(bcmc);
+        $(".ng1").html(vladc);
+        $(".ng2").html(vladc*7);
+    } 
 //checking for butcher being checked
     else if ($('#chkbutch').is(':checked')) {
         plvl = $("#perkLVL").val();
@@ -254,71 +320,6 @@ $("#perkLVL, #chkbutch, #chksmash, #chkmass").on("click change blur focus", func
         $(".ng1").html(vladc);
         $(".ng2").html(vladc*7);
     }
-    //checking for mass and butcher being checked
-    else if ($('#chkbutch').is(':checked') && $('#chkmass').is(':checked')) {
-        plvl = $("#perkLVL").val();
-        plvl = plvl * .01;
-
-        cstcalc1 = cstl + (plvl * cstl) + (butch * cstl) + (mass * cstl);
-        cstcalc2 = csth + (plvl * csth) + (butch * csth);
-        cstcalc3 = cstm + (plvl * cstm) + (butch * cstm);
-        katcalc1 = kat1 + (plvl * kat1) + (butch * kat1) + (mass * kat1);
-        katcalc2 = kat2 + (plvl * kat2) + (butch * kat2);
-        pulcalc1 = pull + (plvl * pull) + (butch * pull) + (mass * pull);
-        pulcalc2 = pulh + (plvl * pulh) + (butch * pulh);
-        pulcalc3 = pulm + (plvl * pulm) + (butch * pulm);
-        zweic1 = zwel + (plvl * zwel) + (butch * zwel) + (mass * zwel);
-        zweic2 = zweh + (plvl * zweh) + (butch * zweh);
-        zweic3 = zwem + (plvl * zwem) + (butch * zwem);
-        evi1c = evil + (plvl * evil) + (butch * evil);
-        evihc = evih + (plvl * evih) + (butch * evih);
-        evimc = evim + (plvl * evim) + (butch * evim);
-        bclc = bcl + (plvl * bcl) + (butch * bcl) + (mass * bcl);
-        bchc = bch + (plvl * bch) + (butch * bch);
-        bcmc = bcm + (plvl * bcm) + (butch * bcm);
-        vladc  = vlad + (plvl * vlad) + (butch * vlad);
-
-        cstcalc1 = Math.round(cstcalc1);
-        cstcalc2 = Math.round(cstcalc2);
-        cstcalc3 = Math.round(cstcalc3);
-        katcalc1 = Math.round(katcalc1);
-        katcalc2 = Math.round(katcalc2);
-        pulcalc1 = Math.round(pulcalc1);
-        pulcalc2 = Math.round(pulcalc2);
-        pulcalc3 = Math.round(pulcalc3);
-        zweic1 = Math.round(zweic1);
-        zweic2 = Math.round(zweic2);
-        zweic3 = Math.round(zweic3);
-        evi1c = Math.round(evi1c);
-        evihc = Math.round(evihc);
-        evimc = Math.round(evimc);
-        bclc = Math.round(bclc);
-        bchc = Math.round(bchc);
-        bcmc = Math.round(bcmc);
-        vladc = Math.round(vladc);
-
-        $(".cst1").html(cstcalc1);
-        $(".cst2").html(cstcalc2);
-        $(".cst3").html(cstcalc3);
-        $(".kat1").html(katcalc1);
-        $(".kat2").html(katcalc2);
-        $(".kat3").html(katcalc1);
-        $(".pulv1").html(pulcalc1);
-        $(".pulv2").html(pulcalc2);
-        $(".pulv3").html(pulcalc3);
-        $(".zwei1").html(zweic1);
-        $(".zwei2").html(zweic2);
-        $(".zwei3").html(zweic3);
-        $(".evis1").html(evi1c);
-        $(".evis2").html(evihc);
-        $(".evis3").html(evimc);
-        $(".bc1").html(bclc);
-        $(".bc2").html(bchc);
-        $(".bc3").html(bcmc);
-        $(".ng1").html(vladc);
-        $(".ng2").html(vladc*7);
-    } 
-
     //checking if massacre only is checked
        else if ($('#chkmass').is(':checked')) {
         plvl = $("#perkLVL").val();
